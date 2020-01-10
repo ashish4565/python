57cf9dd528c0d83f5e846449779bacdb7f3feaef
Ahish_Sharma <aashish.khandal.0000@gmail.com>
2020-01-10 12:20:33 +0530

    add new file

---
+++ age_cal.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  9 11:32:41 2020
+
+@author: ashish
+"""
+
+page=int(input("Enter present age "))
+ayear=int(input("Enter age after "))
+pyear=int(input("Enter age Before "))
+print("What would be your age after",page+ayear,"years from today")
+print("How old were you pyear",page-pyear," years back")
